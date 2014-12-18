84087c40126a35ae5c6e7f59aa39a1ec60dc8f3a
PavelAtlant <icpavelrudko@gmail.com>
2014-12-18 02:36:52 +0200

    Test report style updated.

--- UnitTests/mainTests.c
+++ UnitTests/mainTests.c
@@ -25,19 +25,28 @@ void runTests()
 	tests[0] = deleteClientTest;
 	tests[1] = addCardTest;
 	tests[2] = getClientTest;
-	tests[3] = creditTest;	
+	tests[3] = creditTest;
 	for (int i = 0; i < testsNumber; i++)
 	{		
 		int result = (int)tests[i].action(NULL);
 		if (!result) ok = 0;
-		fprintf(f, "<p>%s: %s</p>\n", tests[i].displayName, result == SUCCESS ? "Passed" : "Failed");
+		fprintf(f, "<p>%s: <span class='%s'>%s</span></p>\n",
+			tests[i].displayName,
+			result == SUCCESS ? "passed" : "failed",
+			result == SUCCESS ? "Passed" : "Failed");
 	}		
+}
+
+void addPrefixToReport()
+{
+	fprintf(f, "<head><style>p{font-family: 'Helvetica Neue', Arial;} .passed{color:green;} .failed{color:red;} </style></head>\n");
 }
 
 int main()
 {
 	InitializeDatabase();
 	fopen_s(&f, "testResults.html", "wt");
+	addPrefixToReport();
 	initializeTests();
 	runTests();
 	fclose(f);
